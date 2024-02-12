c7f1e850fff27c01cc16d4a92ddadfb599ed1aac
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-12 13:20:08 +0100

    Add error message display in PageWelcome component

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -2,18 +2,23 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
 export const PageWelcome = () => {
-	const { message, members } = useContext(AppContext);
+	const { message, members, errorMessage } = useContext(AppContext);
 
 	return (
 		<>
 			<p>{message}</p>
+			{errorMessage && (
+				<p className="mt-3 bg-yellow-300 text-red-700 p-2 rounded w-fit">
+					{errorMessage}
+				</p>
+			)}
 			<h2 className="text-xl mt-3 mb-2">Members</h2>
 			<ul className="list-disc ml-6">
 				{members.map((member) => {
 					return (
 						<li key={member.id}>
 							<p>
-								{member.name} (ID ={member.id.toFixed(3)})
+								{member.name} (ID = {member.id})
 							</p>
 						</li>
 					);
