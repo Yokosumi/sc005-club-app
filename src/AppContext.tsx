ad4cf4f918f5acaeec949135288471d58696e09b
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-12 13:19:15 +0100

    Add zod validation for member objects

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,10 +1,11 @@
 import { createContext, useState, useEffect } from "react";
-import { IMember } from "../interfaces";
+import { IMember, MemberSchema } from "../interfaces";
 import axios from "axios";
 
 interface IAppContext {
 	message: string;
 	members: IMember[];
+	errorMessage: string;
 }
 
 interface IAppProvider {
@@ -17,12 +18,29 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	const [members, setMembers] = useState<IMember[]>([]);
+	const [errorMessage, setErrorMessage] = useState("");
+
 	const message = "Welcome to this site.";
 
 	useEffect(() => {
 		(async () => {
 			const response = await axios.get(`${backendUrl}/members`);
-			const _members = response.data;
+			const _members: IMember[] = [];
+			for (const _member of response.data) {
+				const parseResult = MemberSchema.safeParse(_member);
+				if (parseResult.success) {
+					_members.push(_member);
+				} else {
+					console.log(
+						`LOG ENTRY: bad member object (${JSON.stringify(
+							_member
+						)})`
+					);
+					setErrorMessage(
+						"At least one of the memberss could not be imported, please write to admin@dkjfdkfcompany.com"
+					);
+				}
+			}
 			setMembers(_members);
 		})();
 	}, []);
@@ -32,6 +50,7 @@ export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 			value={{
 				message,
 				members,
+				errorMessage,
 			}}
 		>
 			{children}
